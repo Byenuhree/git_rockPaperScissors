6ff3b6a4db867ba0d67b2d9d7d63ef4411183645
Byenuhree <chavezronneldg@gamil.com>
2023-09-23 21:25:03 +0800

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const choices = ["bato", "papel", "gunting"]
+let winnersLog = []
 
 function game() {
   for (let i = 1; i <= 5; i++) {
-    playRound()
+    playFiveRounds(i)
   }
+  showResult()
 }
 
-function playRound() {
+function playFiveRounds(rounds) {
   const playerSelect = playerChoice()
   const computerSelect = computerChoice()
-  const winPerRound = winnersLog(playerSelect, computerSelect)
-  console.log(playerSelect)
-  console.log(computerSelect)
-  console.log(winPerRound)
+  const winners = winnerConfirmation(playerSelect, computerSelect)
+  winnersLog.push(winners)
+  winnerPerRounds(playerSelect, computerSelect, winners, rounds)
 }
 
 function playerChoice() {
@@ -21,11 +22,11 @@ function playerChoice() {
     inputVal = prompt("Please type: Bato, Papel, or Gunting")
   }
   inputVal = inputVal.toLowerCase()
-  let validCheck = validInput(inputVal)
-  while (validCheck === false) {
+  let elementCheck = arrayChecker(inputVal)
+  while (elementCheck === false) {
     inputVal = prompt("404 please type: Bato, Papel, or Gunting")
     inputVal = inputVal.toLowerCase()
-    validCheck = validInput(inputVal)
+    elementCheck = arrayChecker(inputVal)
   }
   return inputVal
 }
@@ -34,20 +35,38 @@ function computerChoice() {
   return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function validInput(validPram) {
+function arrayChecker(validPram) {
   return choices.includes(validPram)
 }
 
-function winnersLog(player, ai) {
+function winnerConfirmation(player, ai) {
   if (player === ai) {
-    return `Draw: ${player} and ${ai} is same`
+    return "Draw"
   } else if (
     (player === "bato" && ai === "gunting") ||
     (player === "gunting" && ai === "papel") ||
     (player === "papel" && ai === "bato")
   ) {
-    return `Player Win: because the ${player} beat the ${ai}`
+    return "Player"
   } else {
-    return `Computer Win: because the ${ai} beat the ${player}`
+    return "Computer"
   }
+}
+
+function showResult() {
+  let user = winnersLog.filter((element) => element == "Player").length
+  let computer = winnersLog.filter((element) => element == "Computer").length
+  let tie = winnersLog.filter((element) => element == "Draw").length
+  console.log("Result:")
+  console.log("Player Win" + " " + user)
+  console.log("Computer Win" + " " + computer)
+  console.log("Draw Round" + " " + tie)
+}
+
+function winnerPerRounds(playerPick, computerPick, win, round) {
+  console.log("Round:" + " " + round)
+  console.log("Player Choices:" + " " + playerPick)
+  console.log("Computer Choices:" + " " + computerPick)
+  console.log("Winner:" + " " + win)
+  console.log("====================================")
 }
